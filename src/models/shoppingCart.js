66492fa5566c39c2410f0171b3993cca0a188ba9
bruno.guimaraes <bruno.guimaraes@cubos.io>
2019-09-16 10:22:50 -0300

    shoppingCart model association

--- src/models/shoppingCart.js
+++ src/models/shoppingCart.js
@@ -2,10 +2,9 @@ const shoppingCart = (sequelize, DataTypes) => {
 	const ShoppingCart = sequelize.define('shoppingCart', {});
 	ShoppingCart.associate = models => {
 		ShoppingCart.belongsTo(models.User);
-	};
-	ShoppingCart.associate = models => {
 		ShoppingCart.hasMany(models.CartProduct, { onDelete: 'CASCADE' });
 	};
+
 	return ShoppingCart;
 };
 export default shoppingCart;
